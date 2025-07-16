856319a7b379d3d59a9b063d193c00fed774d835
Aldayr28 <crisdair28@gmail.com>
2025-07-16 11:21:31 -0500

    jetstream insertada

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        //User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'admin@gmail.com',
+            'password' => '1234',
         ]);
         $this->call(ProductoSeeder::class);
         $this->call(AccionSeeder::class);
